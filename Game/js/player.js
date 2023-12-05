8dc04c137788c8cc1e3bf684211f7ddc6979660b
Alvaro Guzman <allguzman13@gmail.com>
2023-12-05 17:41:11 -0300

    paginaProducto

--- Game/js/player.js
+++
@@ -1,9 +0,0 @@
-class Player {                      // Clase para construir al player
-    constructor(x, y, width, height) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-    }
-}
-export { Player }
